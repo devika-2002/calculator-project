0fb61f0a39ca8e23d75c4733e4f223c86441b460
Devika Kumari <devika21@navgurukul.org>
2024-10-14 14:58:49 +0530

    Add functionality so that when I click the button, it will display in the input.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,51 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState('')
+  const handleClick=(value)=>{
+    if (value=='CE'){
+      setInputValue('');
+    }else if (value==='Delete'){
+      setInputValue(inputValue.slice(0, -1));
+    }else{
+      setInputValue(inputValue+value);
+    }
+  };
   return (
     <div>
-      <input className='input-col'/>
+      <input className='input-col' value={inputValue}/>
       <div className='cal-div'>
 
         <div className='col-1'>
-          <button className='but-num'>1</button>
-          <button className='but-num'>2</button>
-          <button className='but-num'>3</button>
-          <button className='but-num'>4</button>
-          <button className='but-num1'>CE</button>
+          <button className='but-num' onClick={()=>handleClick('1')}>1</button>
+          <button className='but-num' onClick={()=>handleClick('2')}>2</button>
+          <button className='but-num' onClick={()=>handleClick('3')}>3</button>
+          <button className='but-num' onClick={()=>handleClick('4')}>4</button>
+          <button className='but-num1' onClick={()=>handleClick('CE')}>CE</button>
         </div>
 
         <div className='col-2'>
-          <button className='but-num'>5</button>
-          <button className='but-num'>6</button>
-          <button className='but-num'>7</button>
-          <button className='but-num'>8</button>
-          <button className='but-num'>+</button>
+          <button className='but-num' onClick={()=>handleClick('5')}>5</button>
+          <button className='but-num' onClick={()=>handleClick('6')}>6</button>
+          <button className='but-num' onClick={()=>handleClick('7')}>7</button>
+          <button className='but-num' onClick={()=>handleClick('8')}>8</button>
+          <button className='but-num' onClick={()=>handleClick('+')}>+</button>
         </div>
 
         <div className='col-3'>
-          <button className='but-num'>9</button>
-          <button className='but-num'>0</button>
-          <button className='but-num'>-</button>
-          <button className='but-num'>*</button>
-          <button className='but-num'>/</button>
+          <button className='but-num' onClick={()=>handleClick('9')}>9</button>
+          <button className='but-num' onClick={()=>handleClick('0')}>0</button>
+          <button className='but-num' onClick={()=>handleClick('-')}>-</button>
+          <button className='but-num' onClick={()=>handleClick('*')}>*</button>
+          <button className='but-num' onClick={()=>handleClick('/')}>/</button>
         </div>
 
         <div className='col-4'>
-          <button className='but-num'>%</button>
-          <button className='but-num'>.</button>
-          <button className='but-num'>=</button>
-          <button className='but-num2'>Delete</button>
+          <button className='but-num' onClick={()=>handleClick('%')}>%</button>
+          <button className='but-num' onClick={()=>handleClick('.')}>.</button>
+          <button className='but-num' onClick={()=>handleClick('=')}>=</button>
+          <button className='but-num2' onClick={()=>handleClick('Delete')}>Delete</button>
         </div>
 
       </div>
